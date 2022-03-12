020f278d3c204b5092c321172b129be40839d6e2
jeontigger <boy9609@naver.com>
2022-03-12 21:36:09 +0900

    add: 6th Packet Generator

--- Server/Common/Packet/GenPackets.bat
+++ Server/Common/Packet/GenPackets.bat
@@ -1,5 +1,6 @@
 START ../../PacketGenerator/bin/Debug/net6.0/PacketGenerator.exe ../../PacketGenerator/PDL.xml
 XCOPY /Y GenPackets.cs "../../DummyClient/Packet"
 XCOPY /Y GenPackets.cs "../../Server/Packet"
-
+XCOPY /Y ClientPacketManager.cs "../../DummyClient/Packet"
+XCOPY /Y ServerPacketManager.cs "../../Server/Packet"
 PAUSE
